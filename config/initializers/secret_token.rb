4334904e93c4f03e968beb5fccdfc689287c7a0d
Tony Huang <ttonyhh@gmail.com>
2013-11-07 17:43:30 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Accelwebsite::Application.config.secret_key_base = '78b3af4759e99a6eff45196f30a95a060ba5e0378855b4cd61a73f7a68364110507f1922401fbefc9dfc84d617e126b5cbcd35db335790d3ea89b5f27c0b3b0b'
