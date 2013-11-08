f344c79ddd5117fe07035b7d744e16932efe8582
Tony Huang <ttonyhh@gmail.com>
2013-11-08 16:14:32 -0700

    Created login and signup functionality

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Accelwebsite::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  # note to set this to actual host
+  config.action_mailer.default_url_options = { :host => 'accelsafety.herokuapp.com' }
 end
